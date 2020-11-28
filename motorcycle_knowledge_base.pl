e5b4b860bf79124e17b65633eadf9e63e92439c7
Kasper <kasper.kadzielawa@gmail.com>
2020-11-28 19:41:03 +0100

    Enhance knowledge base

--- motorcycle_knowledge_base.pl
+++ motorcycle_knowledge_base.pl
@@ -13,57 +13,205 @@ motorcycle(adv) :-
   optional(why, travel),
   optional(experience, big),
   optional(distance, long),
-  disqualifying(distance, near).
+  disqualifying(distance, near),
+  optional(roads, paved),
+  optional(roads, gravel),
+  optional(weight, heavy),
+  optional(seat, high),
+  optional(luggage, big),
+  optional(torque, high),
+  optional(riding_position, horse),
+  optional(top_speed, high),
+  optional(fairing, some).
+
+motorcycle(dualsport) :-
+  optional(why, travel),
+  optional(why, fun),
+  optional(experience, small),
+  optional(distance, mid),
+  optional(roads, any),
+  optional(weight, high),
+  optional(seat, mid),
+  optional(luggage, small),
+  optional(torque, mid),
+  optional(riding_position, horse),
+  optional(top_speed, mid),
+  optional(fairing, some).
+
+motorcycle(off_road) :-
+  required(why, fun),
+  optional(experience, no),
+  optional(distance, near),
+  optional(roads, gravel),
+  optional(roads, off),
+  optional(weight, light),
+  optional(seat, high),
+  optional(riding_position, horse),
+  optional(top_speed, low),
+  optional(fairing, no).
 
 motorcycle(cruiser) :-
   optional(why, relax),
+  optional(why, commute),
   optional(experience, mid),
-  optional(distance, long).
+  optional(distance, mid),
+  disqualifying(distance, near),
+  optional(roads, paved),
+  disqualifying(roads, gravel),
+  disqualifying(roads, off),
+  optional(weight, mid),
+  optional(seat, low),
+  optional(luggage, small),
+  optional(torque, high),
+  optional(riding_position, relaxed),
+  optional(top_speed, mid),
+  optional(fairing, no).
+
+motorcycle(touring) :-
+  optional(why, travel),
+  optional(experience, big),
+  optional(distance, long),
+  disqualifying(distance, near),
+  optional(roads, paved),
+  disqualifying(roads, gravel),
+  disqualifying(roads, off),
+  optional(weight, heavy),
+  optional(seat, mid),
+  optional(luggage, big),
+  optional(torque, mid),
+  optional(riding_position, relax),
+  optional(top_speed, high),
+  optional(fairing, full).
+
+motorcycle(sport_touring) :-
+  optional(why, travel),
+  optional(experience, mid),
+  optional(distance, long),
+  disqualifying(distance, near),
+  optional(roads, paved),
+  disqualifying(roads, gravel),
+  disqualifying(roads, off),
+  optional(weight, heavy),
+  optional(seat, mid),
+  optional(luggage, big),
+  optional(torque, big),
+  optional(riding_position, half_sport),
+  optional(top_speed, super_high),
+  optional(fairing, full).
 
 motorcycle(sport) :-
   optional(why, fun),
   optional(experience, mid),
-  optional(distance, mid).
+  optional(distance, mid),
+  optional(roads, paved),
+  disqualifying(roads, gravel),
+  disqualifying(roads, off),
+  optional(weight, mid),
+  optional(seat, high),
+  optional(luggage, no),
+  optional(torque, mid),
+  optional(riding_position, sport),
+  optional(top_speed, super_high),
+  optional(fairing, full).
 
 motorcycle(moped) :-
   optional(why, commute),
   optional(experience, no),
-  optional(distance, near).
+  optional(distance, near),
+  optional(roads, paved),
+  optional(roads, gravel),
+  disqualifying(roads, off),
+  optional(weight, light),
+  optional(seat, low),
+  optional(luggage, small),
+  optional(torque, low),
+  optional(riding_position, horse),
+  optional(top_speed, low),
+  optional(fairing, full),
+  optional(fairing, some).
 
 motorcycle(naked) :-
   optional(why, commute),
   optional(experience, small),
-  optional(distance, mid).
+  optional(distance, mid),
+  optional(roads, paved),
+  disqualifying(roads, gravel),
+  disqualifying(roads, off),
+  optional(weight, mid),
+  optional(seat, mid),
+  optional(luggage, no),
+  optional(torque, mid),
+  optional(riding_position, horse),
+  optional(riding_position, half_sport),
+  optional(top_speed, high),
+  optional(fairing, no).
+
+motorcycle(funbike) :-
+  required(why, fun),
+  optional(experience, no),
+  optional(distance, near),
+  optional(roads, paved),
+  disqualifying(roads, gravel),
+  disqualifying(roads, off),
+  optional(weight, light),
+  optional(seat, low),
+  optional(luggage, no),
+  optional(torque, mid),
+  optional(riding_position, horse),
+  optional(top_speed, low),
+  optional(fairing, no).
 
 % symptom kinds
 required(Type, Value) :-
-  premise(Type, Value).
+  preference(Type, Value).
 
 optional(Type, Value) :-
-  \+ premise(Type, _); premise(Type, Value).
+  \+ preference(Type, _); preference(Type, Value).
 
 no_preference(Type) :-
-  \+ premise(Type, _).
+  \+ preference(Type, _).
 
 disqualifying(Type, Value) :-
-  \+ premise(Type, Value).
+  \+ preference(Type, Value).
 
 % symptoms
-premise(Type, Value) :-
+preference(Type, Value) :-
   ans(Type, Value).
 
-% experience
-premise(experience, mid) :- premise(experience, big).
-premise(experience, small) :- premise(experience, mid).
-premise(experience, no) :- premise(experience, small).
+% experience: big, mid, small, no
+preference(experience, mid) :- preference(experience, big).
+preference(experience, small) :- preference(experience, mid).
+preference(experience, no) :- preference(experience, small).
 
-% distance
-premise(distance, long) :- premise(distance, mid).
-premise(distance, mid) :- premise(distance, near).
+% distance: long, mid, near
+preference(distance, long) :- preference(distance, mid).
+preference(distance, mid) :- preference(distance, near).
 
-% roads
-% premise(roads, paved) :- premise(roads, gravel).
-% premise(roads, gravel) :- premise(roads, no).
+% weight: heavy, mid, light
+preference(weight, mid) :- preference(weight, heavy).
+preference(weight, light) :- preference(weight, mid).
 
-describe(why, travel) :-
-  writeln("I want to travel.").
+% seat_height: high, mid, low
+preference(seat, mid) :- preference(seat, high).
+preference(seat, low) :- preference(seat, mid).
+
+% luggage: big, small, no
+preference(luggage, big) :- preference(luggage, small).
+preference(luggage, small) :- preference(luggage, no).
+
+% torque: high, mid, low
+preference(torque, high) :- preference(torque, mid).
+preference(torque, mid) :- preference(torque, low).
+
+% top_speeds: super_high, high, mid, low
+preference(top_speed, super_high) :- preference(top_speed, high).
+preference(top_speed, high) :- preference(top_speed, mid).
+preference(top_speed, mid) :- preference(top_speed, low).
+
+% roads: paved, grave, off, any
+preference(roads, any) :- true.
+
+% riding_position: horse, sport, half_sport, relaxed
+% why: travel, fun, commute, relax
+% fairing: no, some, full
+
